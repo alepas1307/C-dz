05333507afaf4308428ae7fc3b748c9a8f4d89b7
ale_pashkin <alepas1307@gmail.com>
2022-08-22 03:10:13 +0300

    four_8

---
+++ four_8/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число, до которого нужно проверить четные числа от 1");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int number = 1; number <= N; number++)
+if (number%2==0)
+Console.WriteLine(number);
